fc745804200f4550aadb1d5cdcb8c525034f41f2
Ali78692 <ajharalishaikh@gmail.com>
2021-04-19 13:24:13 +0530

    second commit

--- main.py
+++ main.py
@@ -125,6 +125,6 @@ def delete():
     cv2.destroyAllWindows()
     return render_template('index.html')
 
-# if __name__ == '__main__':
-#     # defining server ip address and port
-#     app.run(host='127.0.0.1',port='5000', debug=True)
+if __name__ == '__main__':
+    # defining server ip address and port
+    app.run(host='127.0.0.1',port='5000', debug=True)
